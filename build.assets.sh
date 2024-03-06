494e462c180009d1bdc5b70a86fcaabc79be03c8
mykohui <39720242+mykohui@users.noreply.github.com>
2024-03-06 11:56:44 +0800

    Update build.assets.sh

--- build.assets.sh
+++ build.assets.sh
@@ -36,7 +36,7 @@ tar -czvf linux_arm_v6_client.tar.gz npc conf/npc.conf conf/multi_account.conf
 
 CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=5 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/npc/npc.go
 
-tar -czvf linux_arm_v5_client.tar.gz npc conf/npc.conf conf/multi_account.conf
+tar -czvf linux_arm_k3_client.tar.gz npc conf/npc.conf conf/multi_account.conf
 
 
 CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/npc/npc.go
@@ -88,7 +88,7 @@ tar -czvf linux_386_server.tar.gz conf/nps.conf conf/tasks.json conf/clients.jso
 
 CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=5 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/nps/nps.go
 
-tar -czvf linux_arm_v5_server.tar.gz conf/nps.conf conf/tasks.json conf/clients.json conf/hosts.json conf/server.key  conf/server.pem web/views web/static nps
+tar -czvf linux_arm_k3_server.tar.gz conf/nps.conf conf/tasks.json conf/clients.json conf/hosts.json conf/server.key  conf/server.pem web/views web/static nps
 
 CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=6 go build -ldflags "-s -w -extldflags -static -extldflags -static" ./cmd/nps/nps.go
 
